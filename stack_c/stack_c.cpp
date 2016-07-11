c6c661a216b5ddfab515e83917fd7c9061c26fdc
shaalx <shaalx@yahoo.com.sg>
2016-07-11 21:44:31 +0800

    auto

---
+++ stack_c/stack_c.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+extern "C" {  
+
+#define true 1
+#define false 0
+
+struct Stack
+{
+	int * base;
+	int * top;
+	int size;
+};
+
+/*
+* 展示栈内容
+*/
+extern void Display(struct Stack stk);
+
+/*
+* 判断栈顶元素距离栈底高度，Go不能做指针运算，需要提供该函数
+*/
+int before(int * a,int* b){
+	return a - b;
+}
+
+/*
+* Go 不能直接索引，需要提供该函数
+*/
+int index(int * base,int length){
+	return base[length];
+}
+
+// 初始化
+void Init(struct Stack * stk,int size){
+	stk->base = new int[size];
+	stk->top = stk->base;
+	stk->size = size;
+}
+
+// 栈容量
+int Size(Stack stk){
+	return stk.size;
+}
+
+// 栈大小
+int Length(struct Stack stk){
+	return before(stk.top,stk.base);
+}
+
+// 是否为空
+bool IsEmpty(struct Stack stk){
+	return Length(stk)<=0;
+}
+
+// 是否满栈
+bool IsFull(struct Stack stk){
+	if (Length(stk) >= Size(stk))
+	{
+		return true;
+	}
+	return false;
+}
+
+// 压栈
+bool Push(struct Stack * stk,int v){
+	if (IsFull(*stk))
+	{
+		return false;
+	}
+	*(stk->top) = v;
+	(stk->top)++;
+	return true;
+}
+
+// 弹栈
+int Pop(struct Stack * stk,bool * ok){
+	if (IsEmpty(*stk))
+	{
+		*ok = false;
+		return -1;
+	}
+	*ok = true;
+	(stk->top)--;
+	return *(stk->top);
+}
+
+// 查看栈顶元素
+int Seek(struct Stack stk,bool * ok){
+if (IsEmpty(stk))
+	{
+		*ok = false;
+		return -1;
+	}
+	*ok = true;
+	return *(stk.top - 1);
+}
+  
+}  
---
+++ stack_c/stack_c.h
@@ -0,0 +1,35 @@
+#ifndef _TEST_H  
+#define _TEST_H  
+  
+#ifdef __cplusplus  
+extern "C" {  
+#endif  
+
+typedef int bool;
+#define true 1
+#define false 0
+struct Stack
+{
+	int * base;
+	int * top;
+	int size;
+};
+
+void Display(struct Stack stk);
+int before(int * a,int* b);
+int index(int * base,int length);
+  
+void Init(struct Stack * stk,int size);
+int Size(struct Stack stk);
+int Length(struct Stack stk);
+bool IsEmpty(struct Stack stk);
+bool IsFull(struct Stack stk);
+bool Push(struct Stack * stk,int v);
+int Pop(struct Stack * stk,bool * ok);
+int Seek(struct Stack stk,bool * ok);
+
+#ifdef __cplusplus  
+}  
+#endif  
+  
+#endif  
